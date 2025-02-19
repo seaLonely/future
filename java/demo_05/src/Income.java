df781426512d308a27618f3bfaf3ef76a2150563
史海川 <shihaichuan@f6car.cn>
2025-02-19 16:52:10 +0800

    2.19

--- java/demo_05/src/Income.java
+++ java/demo_05/src/Income.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Income {
+    protected double income;
+    public double getTax() {
+        return income * 0.1;
+    }
+}
+
+class Salary extends Income {
+    @Override
+    public double getTax() {
+        if (income <= 5000) {
+            return 0;
+        }
+        return (income - 5000) * 0.2;
+    }
+}
+
+class StateCouncilSpecialAllowance extends Income {
+    @Override
+    public double getTax() {
+        return 0;
+    }
 }
