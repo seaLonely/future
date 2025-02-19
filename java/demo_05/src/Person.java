10af30dc82a596fed51d2c0c872ae5f594d95624
史海川 <shihaichuan@f6car.cn>
2025-02-19 14:56:09 +0800

    2.19

--- java/demo_05/src/Person.java
+++ java/demo_05/src/Person.java
@@ -6,4 +6,22 @@ public class Person {
         // this来访问当前的实例，可以解决一些冲突的场景
         this.name = name;
     }
+
+
+    // 方法的重载
+    public void hello() {
+        System.out.println("hello");
+    }
+
+    public void hello (String name) {
+        System.out.println("hello " + name);
+    }
+
+    public void hello (String name, int age) {
+        System.out.println("hello " + name + ", your age is " + age);
+    }
+}
+
+class Student extends Person {
+    private int score;
 }
